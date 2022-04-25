db33ca0a687b409dc2d0ced8f8041aa2613e8e6d
vadim <vadim543@bk.ru>
2022-04-25 19:57:57 +0800

    Add module-01-calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,29 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const objRoot = {
+  name: 'App',
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      symbol: 'sum',
+    };
+  },
+  computed: {
+    result() {
+      switch (this.symbol) {
+        case 'sum':
+          return this.a + this.b;
+        case 'subtract':
+          return this.a - this.b;
+        case 'multiply':
+          return this.a * this.b;
+        case 'divide':
+          return this.a / this.b;
+      }
+    },
+  },
+};
+
+const app = createApp(objRoot);
+app.mount('#app');
