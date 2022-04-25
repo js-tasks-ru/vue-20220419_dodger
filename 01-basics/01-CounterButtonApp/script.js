b0ca3d77f6dd5a58bc07038ac49a6c09e7f5d2fd
vadim <vadim543@bk.ru>
2022-04-25 19:43:26 +0800

    Add module-01-counter

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,19 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const objRoot = {
+  name: 'App',
+  data() {
+    return {
+      value: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.value++;
+    },
+  },
+};
+
+const app = createApp(objRoot);
+
+app.mount('#app');
