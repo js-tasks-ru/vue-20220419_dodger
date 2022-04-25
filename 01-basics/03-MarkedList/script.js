46f945790ca1d8843cf9ed60d78b417bf1d04fc1
vadim <vadim543@bk.ru>
2022-04-25 20:07:39 +0800

    Add module-01-marked-list

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,25 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+const objRoot = {
+  name: 'App',
+  data() {
+    return {
+      search: '',
+      emails,
+    };
+  },
+  computed: {
+    emailsMarked() {
+      return this.emails.map((email) => {
+        return {
+          email,
+          isMarked: this.search && email.includes(this.search),
+        };
+      });
+    },
+  },
+};
+
+const app = createApp(objRoot);
+app.mount('#app');
