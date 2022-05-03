39877d3776d67978c451f9f6cdc7799d9cd69a1e
vadim <vadim543@bk.ru>
2022-05-03 14:31:37 +0800

    Add module-02-meetup-agenda

--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,23 +1,44 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+    },
+  },
+
+  computed: {
+    getPathIcon() {
+      return `/assets/icons/icon-${agendaItemIcons[this.agendaItem.type]}.svg`;
+    },
+    getTitle() {
+      return this.agendaItem.title || agendaItemDefaultTitles[this.agendaItem.type];
+    },
+    getTimeSpending() {
+      return `${this.agendaItem.startsAt} - ${this.agendaItem.endsAt}`;
+    },
+    isTalk() {
+      return this.agendaItem.type === 'talk';
+    },
+  },
+
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="getPathIcon" class="icon" alt="key" />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{ getTimeSpending }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
+        <h3 class="agenda-item__title">{{ getTitle }}</h3>
         <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+          <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span v-if="isTalk" class="agenda-item__lang">{{ agendaItem.language }}</span>
         </p>
-        <p>Description</p>
+        <p v-if="agendaItem.description">{{ agendaItem.description }}</p>
       </div>
     </div>`,
 });
